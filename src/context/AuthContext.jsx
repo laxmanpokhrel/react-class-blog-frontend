b1eee2c0d7d9ae7810d580bfd9a56f665cd1aad3
laxmanpokhrel <laxmanpokhrel102@gmail.com>
2025-02-18 20:00:34 +0545

    feat: persist login state + logout

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,14 +1,22 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 export const AuthContext = createContext();
 
 export default function AuthContextProvider({ children }) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(() =>
+    localStorage.getItem('isLoggedIn') === 'true' ? true : false
+  );
+
   const login = () => {
     setIsLoggedIn(true);
   };
   const logout = () => {
     setIsLoggedIn(false);
   };
+
+  // Persist the login state
+  useEffect(() => {
+    localStorage.setItem('isLoggedIn', String(isLoggedIn));
+  }, [isLoggedIn]);
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
--- src/pages/CreateBlog/index.jsx
+++ src/pages/CreateBlog/index.jsx
@@ -1,3 +1,23 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+
 export default function CreateBlog() {
-  return <div>Create blog</div>;
+  const { logout } = useContext(AuthContext);
+
+  const logoutHandler = () => {
+    localStorage.removeItem('token');
+    logout();
+  };
+
+  return (
+    <div>
+      <button
+        className="cursor-pointer px-2 py-1 p-2 bg-gray-200 rounded-lg "
+        type="button"
+        onClick={logoutHandler}
+      >
+        Logout
+      </button>
+    </div>
+  );
 }
